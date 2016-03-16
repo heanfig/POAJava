9b3b3b65f2aff1a5f17ea555bb8398ed0bc93e5d
Herman Andres <heanfig@gmail.com>
2016-03-16 10:26:09 -0500

    Initial Commit

---
+++ src/com/remington/ejercicios/General.java
@@ -0,0 +1,14 @@
+package com.remington.ejercicios;
+
+public class General {
+
+	public static void main(String[] args) {
+		General g = new General();
+		g.demo("");
+	}
+	
+	public void demo(String nombre){
+		System.out.println("ejecuccion");
+	}
+
+}
